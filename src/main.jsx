0659ce1cb170697e3603acdf409bd472d253160e
Richard Villasenor <richard.c.villasenor@gmail.com>
2023-10-28 22:16:41 -0700

    feat: routing

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,30 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import Home from "./pages/Home.jsx";
+import Contact from "./pages/Contact.jsx";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const router = createBrowserRouter([
+  {
+    path: "/gtldr_v1/",
+    element: <App />,
+    children: [
+      {
+        path: "/gtldr_v1/",
+        element: <Home />,
+      },
+      {
+        path: "/gtldr_v1/contact",
+        element: <Contact />,
+      },
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
