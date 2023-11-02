21f15bcae4b0b2f3e81190fdfe3dfe171cf68ce2
Richard Villasenor <richard.c.villasenor@gmail.com>
2023-11-01 23:38:10 -0700

    test: logo image handling

--- src/www/modules/General/Components/TopNav.jsx
+++ src/www/modules/General/Components/TopNav.jsx
@@ -1,5 +1,5 @@
-// import "../../../../index.css";
-import "/src/index.css";
+import "../../../../index.css";
+// import "/src/index.css";
 import { Link } from "react-router-dom";
 import { useState } from "react";
 
@@ -8,7 +8,7 @@ export default function TopNav() {
     <>
       <nav className="flex flex-row justify-between" style={{borderBottom: "1px solid black"}}>
         <Link to="/gtldr_v1/" className="flex items-center">
-          <img src="src\assets\logo.png" alt="site logo" style={{width: "20%"}}/>
+          <img src="../Assets/logo.png" alt="site logo" style={{width: "20%"}}/>
           <span className="logo-font font-extrabold text-3xl">G-TL;DR</span>
         </Link>
         <div className="flex flex-row items-center">
