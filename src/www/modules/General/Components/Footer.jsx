28b74a510ce08fb6feebae3e46b9bb0a87bde4c1
Richard Villasenor <richard.c.villasenor@gmail.com>
2023-11-03 12:01:56 -0700

    update: footer scaffolding

--- src/www/modules/General/Components/Footer.jsx
+++ src/www/modules/General/Components/Footer.jsx
@@ -1,9 +1,10 @@
-
-
 export default function Footer() {
   return (
-    <>
-        <div className="bg-palette2">test</div>
-    </>
+    <div className="bg-palette2">
+      <div>Site Links</div>
+      <div>links go here</div>
+      <div>© 2023 Richard Villaseñor</div>
+      <div>© All rights reserved by Hoyoverse. Other properties belong to their respective owners.</div>
+    </div>
   );
 }
---
+++ src/www/modules/General/Components/TestText.jsx
@@ -0,0 +1,202 @@
+export default function TestText() {
+    return (
+        <div className="mx-3 my-3">
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Animi vero
+        minima perspiciatis facilis a vitae necessitatibus enim, recusandae
+        eaque iusto laborum nobis sunt non fugit quia temporibus nulla itaque
+        porro.
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Animi vero
+        minima perspiciatis facilis a vitae necessitatibus enim, recusandae
+        eaque iusto laborum nobis sunt non fugit quia temporibus nulla itaque
+        porro.
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Animi vero
+        minima perspiciatis facilis a vitae necessitatibus enim, recusandae
+        eaque iusto laborum nobis sunt non fugit quia temporibus nulla itaque
+        porro.
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Animi vero
+        minima perspiciatis facilis a vitae necessitatibus enim, recusandae
+        eaque iusto laborum nobis sunt non fugit quia temporibus nulla itaque
+        porro.
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Animi vero
+        minima perspiciatis facilis a vitae necessitatibus enim, recusandae
+        eaque iusto laborum nobis sunt non fugit quia temporibus nulla itaque
+        porro.
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Animi vero
+        minima perspiciatis facilis a vitae necessitatibus enim, recusandae
+        eaque iusto laborum nobis sunt non fugit quia temporibus nulla itaque
+        porro.
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Animi vero
+        minima perspiciatis facilis a vitae necessitatibus enim, recusandae
+        eaque iusto laborum nobis sunt non fugit quia temporibus nulla itaque
+        porro.
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Animi vero
+        minima perspiciatis facilis a vitae necessitatibus enim, recusandae
+        eaque iusto laborum nobis sunt non fugit quia temporibus nulla itaque
+        porro.
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Animi vero
+        minima perspiciatis facilis a vitae necessitatibus enim, recusandae
+        eaque iusto laborum nobis sunt non fugit quia temporibus nulla itaque
+        porro.
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Animi vero
+        minima perspiciatis facilis a vitae necessitatibus enim, recusandae
+        eaque iusto laborum nobis sunt non fugit quia temporibus nulla itaque
+        porro.
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Animi vero
+        minima perspiciatis facilis a vitae necessitatibus enim, recusandae
+        eaque iusto laborum nobis sunt non fugit quia temporibus nulla itaque
+        porro.
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Animi vero
+        minima perspiciatis facilis a vitae necessitatibus enim, recusandae
+        eaque iusto laborum nobis sunt non fugit quia temporibus nulla itaque
+        porro.
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Animi vero
+        minima perspiciatis facilis a vitae necessitatibus enim, recusandae
+        eaque iusto laborum nobis sunt non fugit quia temporibus nulla itaque
+        porro.
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Animi vero
+        minima perspiciatis facilis a vitae necessitatibus enim, recusandae
+        eaque iusto laborum nobis sunt non fugit quia temporibus nulla itaque
+        porro.
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Animi vero
+        minima perspiciatis facilis a vitae necessitatibus enim, recusandae
+        eaque iusto laborum nobis sunt non fugit quia temporibus nulla itaque
+        porro.
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Animi vero
+        minima perspiciatis facilis a vitae necessitatibus enim, recusandae
+        eaque iusto laborum nobis sunt non fugit quia temporibus nulla itaque
+        porro.
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Animi vero
+        minima perspiciatis facilis a vitae necessitatibus enim, recusandae
+        eaque iusto laborum nobis sunt non fugit quia temporibus nulla itaque
+        porro.
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Animi vero
+        minima perspiciatis facilis a vitae necessitatibus enim, recusandae
+        eaque iusto laborum nobis sunt non fugit quia temporibus nulla itaque
+        porro.
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Animi vero
+        minima perspiciatis facilis a vitae necessitatibus enim, recusandae
+        eaque iusto laborum nobis sunt non fugit quia temporibus nulla itaque
+        porro.
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Animi vero
+        minima perspiciatis facilis a vitae necessitatibus enim, recusandae
+        eaque iusto laborum nobis sunt non fugit quia temporibus nulla itaque
+        porro.
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Animi vero
+        minima perspiciatis facilis a vitae necessitatibus enim, recusandae
+        eaque iusto laborum nobis sunt non fugit quia temporibus nulla itaque
+        porro.
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Animi vero
+        minima perspiciatis facilis a vitae necessitatibus enim, recusandae
+        eaque iusto laborum nobis sunt non fugit quia temporibus nulla itaque
+        porro.
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Animi vero
+        minima perspiciatis facilis a vitae necessitatibus enim, recusandae
+        eaque iusto laborum nobis sunt non fugit quia temporibus nulla itaque
+        porro.
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Animi vero
+        minima perspiciatis facilis a vitae necessitatibus enim, recusandae
+        eaque iusto laborum nobis sunt non fugit quia temporibus nulla itaque
+        porro.
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Animi vero
+        minima perspiciatis facilis a vitae necessitatibus enim, recusandae
+        eaque iusto laborum nobis sunt non fugit quia temporibus nulla itaque
+        porro.
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Animi vero
+        minima perspiciatis facilis a vitae necessitatibus enim, recusandae
+        eaque iusto laborum nobis sunt non fugit quia temporibus nulla itaque
+        porro.
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Animi vero
+        minima perspiciatis facilis a vitae necessitatibus enim, recusandae
+        eaque iusto laborum nobis sunt non fugit quia temporibus nulla itaque
+        porro.
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Animi vero
+        minima perspiciatis facilis a vitae necessitatibus enim, recusandae
+        eaque iusto laborum nobis sunt non fugit quia temporibus nulla itaque
+        porro.
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Animi vero
+        minima perspiciatis facilis a vitae necessitatibus enim, recusandae
+        eaque iusto laborum nobis sunt non fugit quia temporibus nulla itaque
+        porro.
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Animi vero
+        minima perspiciatis facilis a vitae necessitatibus enim, recusandae
+        eaque iusto laborum nobis sunt non fugit quia temporibus nulla itaque
+        porro.
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Animi vero
+        minima perspiciatis facilis a vitae necessitatibus enim, recusandae
+        eaque iusto laborum nobis sunt non fugit quia temporibus nulla itaque
+        porro.
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Animi vero
+        minima perspiciatis facilis a vitae necessitatibus enim, recusandae
+        eaque iusto laborum nobis sunt non fugit quia temporibus nulla itaque
+        porro.
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Animi vero
+        minima perspiciatis facilis a vitae necessitatibus enim, recusandae
+        eaque iusto laborum nobis sunt non fugit quia temporibus nulla itaque
+        porro.
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Animi vero
+        minima perspiciatis facilis a vitae necessitatibus enim, recusandae
+        eaque iusto laborum nobis sunt non fugit quia temporibus nulla itaque
+        porro.
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Animi vero
+        minima perspiciatis facilis a vitae necessitatibus enim, recusandae
+        eaque iusto laborum nobis sunt non fugit quia temporibus nulla itaque
+        porro.
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Animi vero
+        minima perspiciatis facilis a vitae necessitatibus enim, recusandae
+        eaque iusto laborum nobis sunt non fugit quia temporibus nulla itaque
+        porro.
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Animi vero
+        minima perspiciatis facilis a vitae necessitatibus enim, recusandae
+        eaque iusto laborum nobis sunt non fugit quia temporibus nulla itaque
+        porro.
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Animi vero
+        minima perspiciatis facilis a vitae necessitatibus enim, recusandae
+        eaque iusto laborum nobis sunt non fugit quia temporibus nulla itaque
+        porro.
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Animi vero
+        minima perspiciatis facilis a vitae necessitatibus enim, recusandae
+        eaque iusto laborum nobis sunt non fugit quia temporibus nulla itaque
+        porro.
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Animi vero
+        minima perspiciatis facilis a vitae necessitatibus enim, recusandae
+        eaque iusto laborum nobis sunt non fugit quia temporibus nulla itaque
+        porro.
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Animi vero
+        minima perspiciatis facilis a vitae necessitatibus enim, recusandae
+        eaque iusto laborum nobis sunt non fugit quia temporibus nulla itaque
+        porro.
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Animi vero
+        minima perspiciatis facilis a vitae necessitatibus enim, recusandae
+        eaque iusto laborum nobis sunt non fugit quia temporibus nulla itaque
+        porro.
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Animi vero
+        minima perspiciatis facilis a vitae necessitatibus enim, recusandae
+        eaque iusto laborum nobis sunt non fugit quia temporibus nulla itaque
+        porro.
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Animi vero
+        minima perspiciatis facilis a vitae necessitatibus enim, recusandae
+        eaque iusto laborum nobis sunt non fugit quia temporibus nulla itaque
+        porro.
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Animi vero
+        minima perspiciatis facilis a vitae necessitatibus enim, recusandae
+        eaque iusto laborum nobis sunt non fugit quia temporibus nulla itaque
+        porro.
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Animi vero
+        minima perspiciatis facilis a vitae necessitatibus enim, recusandae
+        eaque iusto laborum nobis sunt non fugit quia temporibus nulla itaque
+        porro.
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Animi vero
+        minima perspiciatis facilis a vitae necessitatibus enim, recusandae
+        eaque iusto laborum nobis sunt non fugit quia temporibus nulla itaque
+        porro.
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Animi vero
+        minima perspiciatis facilis a vitae necessitatibus enim, recusandae
+        eaque iusto laborum nobis sunt non fugit quia temporibus nulla itaque
+        porro.
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Animi vero
+        minima perspiciatis facilis a vitae necessitatibus enim, recusandae
+        eaque iusto laborum nobis sunt non fugit quia temporibus nulla itaque
+        porro.
+      </div>
+    );
+}
