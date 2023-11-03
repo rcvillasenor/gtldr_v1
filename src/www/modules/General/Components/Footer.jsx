5be4e4e18a10015ea9d10aa5a5265a6f0eb63d39
Richard Villasenor <richard.c.villasenor@gmail.com>
2023-11-03 12:11:49 -0700

    test: commit

--- src/www/modules/General/Components/Footer.jsx
+++ src/www/modules/General/Components/Footer.jsx
@@ -1,10 +1,10 @@
 export default function Footer() {
   return (
     <div className="bg-palette2">
-      <div>Site Links</div>
-      <div>links go here</div>
-      <div>© 2023 Richard Villaseñor</div>
-      <div>© All rights reserved by Hoyoverse. Other properties belong to their respective owners.</div>
+      <div className="mx-3 my-3">Site Links</div>
+      <div className="mx-3 my-3">links go here</div>
+      <div className="text-center">© 2023 Richard Villaseñor</div>
+      <div className="text-center">© All rights reserved by Hoyoverse. Other properties belong to their respective owners.</div>
     </div>
   );
 }
